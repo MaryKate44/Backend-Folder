9d738de21c467c8ea014189033f2e5ed3cf6dbdb
MaryKate44 <kateomigie44@gmail.com>
2025-03-14 10:54:14 +0100

    updates

--- Assignment.js
+++ Assignment.js
@@ -1,3 +1,10 @@
+/*ACTIVITIES:
+2a. Define a loop which will print out 1 - 100.
+2b. Define a loop which will print out 1 - 10 to the console without printing out the numbers 4 and 5.
+3. Define a function which adds the number values that the function receives and print the result in your console.
+4. Define a function “greet” that prints “Hello name, welcome to Deebug”. Where name is the argument that was passed as a parameter to the greet function.
+5. Using any loop of your choice, write a program that prints of the multiples of 6 */
+
 //Question 4
 function greet(name){
     console.log(`Hello ${name}, Welcome to Deebug`);
